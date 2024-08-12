4763f5a6f3554b1ea4178c7df2f6548fee1e9ba5
SuminAnn <annsumin2066@naver.com>
2024-08-12 17:18:12 +0900

    toString, equals

---
+++ src/lang/object/equals/test/Rectangle.java
@@ -0,0 +1,34 @@
+package lang.object.equals.test;
+
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [width=" + width + ", height=" + height + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Rectangle other = (Rectangle) obj;
+        if (width != other.width)
+            return false;
+        if (height != other.height)
+            return false;
+        return true;
+    }
+    
+}
