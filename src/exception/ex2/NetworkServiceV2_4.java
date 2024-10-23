799138a64242b679cb495329425c5b0eb5ecda40
SuminAnn <annsumin2066@naver.com>
2024-10-23 17:36:55 +0900

    Exception4

---
+++ src/exception/ex2/NetworkServiceV2_4.java
@@ -0,0 +1,22 @@
+package exception.ex2;
+
+public class NetworkServiceV2_4 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV2 client = new NetworkClientV2(address);
+        client.initError(data);
+        
+        try {
+            client.connect();
+            client.send(data);
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메시지: " + e.getMessage());
+        }
+        
+        client.disconnect();
+    }
+}
+/**
+ * try catch 밖에 disconnect()를 처리하는 경우 catch에서 잡을 수 없는 예외가 발생할 때 문제가 발생한다
+ */
